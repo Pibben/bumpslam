ff66b77cbe004848d8ea56cebe44a8762df86038
Per Zetterlund <per@pz.se>
2018-06-16 08:31:20 +0200

    Updates

--- main.py
+++ main.py
@@ -3,28 +3,41 @@ import numpy as np
 import cv2
 
 
+def draw_box(box, canvas, color, thickness):
+    box2 = cv2.boxPoints(box)
+    box3 = np.int0(box2)
+    cv2.drawContours(canvas, [box3], 0, color, thickness)
+
+
 class Agent:
     def __init__(self, x, y, angle):
-        self.position = np.array([x,y])
+        self.position = np.array([x, y])
         self.heading = angle
         self.width = 25
         self.height = 40
-        self.speed = np.zeros((1, 2))
+        self.speed = 15
+
+    def get_box(self):
+        return self.position, (self.height, self.width), np.degrees(self.heading)
 
     def draw_box(self, canvas):
-        box = (self.position, (self.width, self.height), self.heading)
-        box2 = cv2.boxPoints(box)
-        box3 = np.int0(box2)
-        cv2.drawContours(canvas, [box3], 0, (0, 191, 255), 1)
+        box = self.get_box()
+        draw_box(box, canvas, (0, 191, 255), 1)
 
     def draw_point(self, canvas):
         pass
 
     def move(self):
-        self.position += self.speed
+        self.position += self.speed * np.array([math.cos(self.heading), math.sin(self.heading)])
 
     def rotate(self, angle_offset):
-        self.heading = (self.heading + angle_offset) % math.pi
+        self.heading = (self.heading + angle_offset) % (2.0 * math.pi)
+
+    def reverse(self):
+        self.speed = -self.speed
+
+    def set_speed(self, speed):
+        self.speed = speed
 
 
 class Environment:
@@ -32,13 +45,46 @@ class Environment:
         self.width = 768
         self.height = 512
 
-        self.cells = np.zeros((self.width, self.height))
+        self.cells = np.zeros((self.height, self.width))
+        cv2.rectangle(self.cells, (20, 20), (self.width - 20, self.height - 20), 1.0)
 
     def draw(self, canvas):
-        pass
+        color_img = cv2.cvtColor((255.0 * self.cells).astype(np.uint8), cv2.COLOR_GRAY2RGB)
+        canvas[:] = np.maximum(color_img, canvas)
 
     def get(self, box):
-        pass
+        points = cv2.boxPoints(box)
+        bb = cv2.boundingRect(points)
+        roi = self.cells[bb[1]:bb[1] + bb[3], bb[0]:bb[0] + bb[2]]
+        mask = np.full_like(roi, 0)
+        adjusted_box = ((box[0][0] - bb[0], box[0][1] - bb[1]), box[1], box[2])
+
+        draw_box(adjusted_box, mask, 1.0, -1)
+
+        return np.sum(np.multiply(roi, mask))
+
+
+class Estimation:
+    def __init__(self):
+        self.width = 768
+        self.height = 512
+
+        self.cells = np.zeros((self.height, self.width))
+
+    def draw(self, canvas):
+        color_img = cv2.cvtColor((255.0 * self.cells).astype(np.uint8), cv2.COLOR_GRAY2RGB)
+        canvas[:] = np.maximum(color_img, canvas)
+
+    def get(self, box):
+        points = cv2.boxPoints(box)
+        bb = cv2.boundingRect(points)
+        roi = self.cells[bb[1]:bb[1] + bb[3], bb[0]:bb[0] + bb[2]]
+        mask = np.full_like(roi, 0)
+        adjusted_box = ((box[0][0] - bb[0], box[0][1] - bb[1]), box[1], box[2])
+
+        draw_box(adjusted_box, mask, 1.0, -1)
+
+        return np.sum(np.multiply(roi, mask))
 
     def set(self, box, value):
         pass
@@ -51,14 +97,26 @@ class Environment:
 
 
 def main():
-    img = np.zeros((512, 768, 3), np.int8)
+    img = np.ones((512, 768, 3), np.uint8)
 
-    a = Agent(100, 100, 0)
+    e = Environment()
+    a = Agent(100.0, 100.0, np.radians(135))
 
-    a.draw_box(img)
+    for i in range(200):
+        img.fill(0)
+        e.draw(img)
+        a.move()
+        a.draw_box(img)
 
-    cv2.imshow('Draw01', img)
-    cv2.waitKey(0)
+        cv2.imshow('Draw01', img)
+        cv2.waitKey(100)
+
+        if e.get(a.get_box()) > 0:
+            a.reverse()
+            for r in range(int(45.0 / abs(a.speed))):
+                a.move()
+            a.rotate(np.random.uniform(np.radians(135), np.radians(135+90)))
+            a.reverse()
 
 
 if __name__ == "__main__":
