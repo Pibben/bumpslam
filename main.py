3e3557fd9b3d03eb1255e57447150dd398c63260
Per Zetterlund <per@pz.se>
2018-06-21 18:45:23 +0200

    Kind of working localisation

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ def draw_box(box, canvas, color, thickness):
     cv2.drawContours(canvas, [box3], 0, color, thickness)
 
 
+def calc_roi_and_mask_circle(center, radius, img):
+    center = np.int0(center)
+    roi = img[center[1] - radius:center[1] + radius, center[0] - radius:center[0] + radius]
+    mask = np.full_like(roi, 0)
+
+    cv2.circle(mask, (radius, radius), radius, 1.0, -1)
+
+    return roi, mask
+
+
 def calc_roi_and_mask(points, img):
     bb = cv2.boundingRect(points)
     roi = img[bb[1]:bb[1] + bb[3], bb[0]:bb[0] + bb[2]]
@@ -24,6 +34,7 @@ def calc_roi_and_mask(points, img):
 
 def sus(agents, number_to_keep):
     total = sum(a.fitness for a in agents)
+    assert total > 0
     dist = total / number_to_keep
     start = np.random.uniform(0.0, dist)
     pointers = [start + i * dist for i in range(number_to_keep)]
@@ -35,18 +46,21 @@ def sus(agents, number_to_keep):
         while so_far < p:
             i += 1
             so_far += agents[i].fitness
-        keep.append(agents[i].clone_and_reset())
+        new_agent = agents[i].clone()
+        new_agent.reset()
+        keep.append(new_agent)
 
     return keep
 
 
 class Agent:
-    def __init__(self, x, y, angle):
+    def __init__(self, x, y, angle, width=25, height=40, speed=5, fitness=1):
         self.position = np.array([x, y])
         self.heading = angle
-        self.width = 25
-        self.height = 40
-        self.speed = 5
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.fitness = fitness
 
     def get_box(self):
         return self.position, (self.height, self.width), np.degrees(self.heading)
@@ -78,6 +92,17 @@ class Agent:
     def set_speed(self, speed):
         self.speed = speed
 
+    def clone(self):
+        return Agent(self.position[0], self.position[1], self.heading, self.width, self.heading,
+                     self.speed, self.fitness)
+
+    def reset(self):
+        self.fitness = 1
+        self.rotate(np.random.uniform(np.radians(-2), np.radians(2)))
+
+    def reduce_fitness(self):
+        self.fitness = 0
+
 
 class Environment:
     def __init__(self):
@@ -94,6 +119,11 @@ class Environment:
 
     def get(self, points):
         roi, mask = calc_roi_and_mask(points, self.cells)
+
+        return np.sum(np.multiply(roi, mask))
+
+    def get_circle(self, center, radius):
+        roi, mask = calc_roi_and_mask_circle(center, radius, self.cells)
 
         return np.sum(np.multiply(roi, mask))
 
@@ -130,41 +160,88 @@ class Estimation:
         roi[:] = np.clip(roi, 0.0, 1.0)
 
 
-def main():
-    img = np.ones((512, 768, 3), np.uint8)
+class Simulation:
+    def __init__(self):
+        self.num_hypotheses = 1000
 
-    e = Environment()
-    ee = Estimation()
+        self.img = np.ones((512, 768, 3), np.uint8)
 
-    a_s = [Agent(768 / 2, 512 / 2, np.random.uniform(0, 2*np.pi)) for _ in range(1000)]
+        self.env = Environment()
+        self.est = Estimation()
 
-    while True:
-        img.fill(0)
+        self.pilot = Agent(768 / 2, 512 / 2, np.random.uniform(0, 2 * np.pi))
 
-        for a in a_s:
-            last_corners = a.get_front_corners()
-            a.move()
-            new_corners = a.get_front_corners()
+        self.hypotheses = [Agent(768 / 2, 512 / 2, np.random.uniform(0, 2*np.pi)) for _ in range(self.num_hypotheses)]
 
-            contour = np.concatenate((last_corners, new_corners[::-1]))
+    def move_and_get_contour(self, agent):
+        last_corners = agent.get_front_corners()
+        agent.move()
+        new_corners = agent.get_front_corners()
 
-            if e.get(contour) > 0:
-                ee.increase(contour, 0.02)
-                a.reverse()
-                for r in range(int(45.0 / abs(a.speed))):
-                    a.move()
-                a.rotate(np.random.uniform(np.radians(135), np.radians(135+90)))
-                a.reverse()
+        return np.concatenate((last_corners, new_corners[::-1]))
+
+    def is_collision(self, contour):
+        return self.env.get(contour) > 1
+
+    def is_collision_circle(self, center, radius):
+        return self.env.get_circle(center, radius) > 1
+
+    def random_angle(self):
+        return np.random.uniform(np.radians(135), np.radians(135 + 90))
+
+    def turn_around(self, agent, angle):
+        agent.reverse()
+        for r in range(int(25.0 / abs(agent.speed))):
+            agent.move()
+        agent.rotate(angle)
+        agent.reverse()
+
+    def handle_hypothesis_collision(self, agent, contour, angle):
+        self.est.increase(contour, 0.02)
+        self.turn_around(agent, angle)
+
+    def handle_hypothesis_miss(self, agent, contour):
+        return self.est.decrease(contour, 0.02)
+
+    def run(self):
+        while True:
+            self.img.fill(0)
+
+            contour = self.move_and_get_contour(self.pilot)
+            if self.is_collision(contour):
+                angle = self.random_angle()
+                self.turn_around(self.pilot, angle)
+
+                for agent in self.hypotheses:
+                    contour = self.move_and_get_contour(agent)
+                    self.handle_hypothesis_collision(agent, contour, angle)
+
+                    if not self.is_collision_circle(agent.position, 50):
+                        agent.reduce_fitness()
+
             else:
-                ee.decrease(contour, 0.02)
+                for agent in self.hypotheses:
+                    contour = self.move_and_get_contour(agent)
+                    self.handle_hypothesis_miss(agent, contour)
 
-            a.draw_point(img)
-        ee.draw(img)
+                    if self.is_collision_circle(agent.position, 20):
+                        agent.reduce_fitness()
 
-        #e.draw(img)
+            self.hypotheses = sus(self.hypotheses, self.num_hypotheses)
 
-        cv2.imshow('Draw01', img)
-        cv2.waitKey(1)
+            for agent in self.hypotheses:
+                agent.draw_point(self.img)
+
+            self.pilot.draw_box(self.img)
+            self.env.draw(self.img)
+            #self.est.draw(self.img)
+
+            cv2.imshow('Draw01', self.img)
+            cv2.waitKey(1)
+
+def main():
+    sim = Simulation()
+    sim.run()
 
 
 if __name__ == "__main__":
